36b9ac8488f6533d4167a52c0bd9c5148fdd5413
WALIDMO23 <121633061+WALIDMO23@users.noreply.github.com>
2022-12-29 21:05:51 +0200

    Add files via upload

---
+++ Perfect Number.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PerfectNumber
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+       { 
+    
+    int s,e,w=0 ;
+    
+       Console.WriteLine("Enter The Start : ");
+       s = int.Parse(Console.ReadLine());
+       
+       Console.WriteLine("Enter The End : ");
+       e = int.Parse(Console.ReadLine());
+        
+        for (int n = s ; n <= e; n++)
+        { 
+        	if (n<=0) 
+        	continue;
+        	
+        	w = 0 ;
+        	
+        	for (int x = 1 ; x<n ; x++)
+        	{
+        	 if  (n%x==0) 
+              
+        	  {
+        	  	    w += x; 
+           	  }
+        	}
+           	  if (w==n)
+        	 	{
+        	 		Console.WriteLine("\t"+n);
+        	 	}
+        	}
+        	 	
+        	
+        	
+        
+        Console.ReadKey();
+     
+          
+  	 }	
+  }
+}
---
+++ Prime Number.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PRIME_NUMBER
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+   {
+    double x,y,e=0  ;
+   
+    Console.WriteLine("Enter The START : ");
+    x = double.Parse(Console.ReadLine());
+   
+    Console.WriteLine("Enter The END : ");
+    y = double.Parse(Console.ReadLine());
+
+     for(double z=x ;z <= y ; z++ )
+	        { 
+	        	for (int w = 1 ; w < y ;w++) 
+	        	{
+	        		if (z%w==0)
+	        {
+	        	e++ ;
+	        }
+	        	}
+	        if (e==2) 
+	       {
+	        Console.WriteLine(z);
+	        }	
+	        	
+	       e=0 ; 	
+	        }
+	      
+	   
+     Console.ReadKey();		
+		}
+	}	
+	
